ec837774c9ac21e4170f358f4ec685bc833e5fc7
Aggas, Joshua <Joshua.Aggas@ddiworld.com>
2022-05-10 14:01:37 -0400

    gatsby node fix for third party modules window error

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+//We're using an NPM module that expects window to be defined which causes a failure at gatsby 'build'. This fixes that.
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html" || stage === "develop-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /bad-module/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
