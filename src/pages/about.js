a62d4ee413128d58237c7e6f8e681f785fdb1694
Aggas, Joshua <Joshua.Aggas@ddiworld.com>
2022-05-11 09:02:33 -0400

    forgot to add about boiler plate

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,2 +1,12 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+
+class About extends React.Component{
+    render(){
+        return(
+            <p>About Page placeholder</p>
+        )
+    }
+}
+
+export default About;
