1654097f298c37ea4d37b406676614e90c29231f
Aggas, Joshua <Joshua.Aggas@ddiworld.com>
2022-05-11 12:58:12 -0400

    debugging

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,9 @@ export default function Layout({children}){
     const defaultDark = typeof window !== 'undefined' ? window.matchMedia('(prefers-color-scheme:dark)').matches : false;
     const persistedTheme = typeof window !== 'undefined' ? window.localStorage.getItem('theme') : null;
     
+    useEffect(() => {
+        console.log(theme)
+    })
     const [theme, setTheme] = useLocalStorage('theme', persistedTheme !== null ? persistedTheme : defaultDark === true ? 'dark' :' light')
     const themeIcon = React.useRef();
     const lowerThemeIcon = (e) => {
